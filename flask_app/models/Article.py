7dd5cef2135aca6b57f51ae515e39d40c7c205e0
AARON JOHNSON <jardaaroncs@gmail.com>
2023-09-07 10:15:47 -0700

    add annotations/docstrings

--- flask_app/models/Article.py
+++ flask_app/models/Article.py
@@ -14,6 +14,12 @@ class Article:
 
     @classmethod
     def get_all(cls) -> list[object]:
+        """
+        Retrieve all articles from the database.
+
+            Returns:
+                list[object]: A list of Article objects representing all articles in the database.
+        """
         query_results = supa.table(TABLE).select('*').execute()
         articles = []
 
@@ -24,7 +30,17 @@ class Article:
 
     @classmethod
     def get_one(cls, article_id: int) -> object:
-        query_results = supa.table(TABLE).select('*').eq('id', article_id).execute()
+        """
+        Retrieve a single article from the database by its ID.
+
+            Args:
+                article_id (int): The ID of the article to retrieve.
+
+            Returns:
+                object: An Article object representing the retrieved article.
+        """
+        query_results = supa.table(TABLE).select(
+            '*').eq('id', article_id).execute()
         articles = query_results.data
         article = cls(articles[0])
 
@@ -32,8 +48,18 @@ class Article:
 
     @classmethod
     def get_by_team(cls, team: str) -> list[object]:
+        """
+        Retrieve articles by the team they belong to.
+
+            Args:
+                team (str): The name of the team.
+
+            Returns:
+                list[object]: A list of Article objects belonging to the specified team.
+        """
         team = team.title()
-        query_results = supa.table(TABLE).select('*').eq('team_name', team).execute()
+        query_results = supa.table(TABLE).select(
+            '*').eq('team_name', team).execute()
         articles = []
 
         for article in query_results.data:
@@ -43,6 +69,15 @@ class Article:
 
     @classmethod
     def get_by_column(cls, column: str) -> list[object]:
+        """
+        Retrieve articles based on a specific column name.
+
+            Args:
+                column (str): The name of the column to search for.
+
+            Returns:
+                list[object]: A list of Article objects containing data from the specified column.
+        """
         column = column.lower()
         query_results = supa.table(TABLE).select(column).execute()
         articles = []
@@ -55,6 +90,15 @@ class Article:
 
     @classmethod
     def save(cls, article_list: list | dict[str, str | int]) -> list[dict[str, str | int]]:
+        """
+        Save articles to the database, avoiding duplicates.
+
+            Args:
+                article_list (list | dict[str, str | int]): A list of article dictionaries or a single article dictionary.
+
+            Returns:
+                list[dict[str, str | int]]: A list of dictionaries representing the saved articles' data.
+        """
         filtered_articles = cls.remove_duplicate_articles(article_list)
         data, count = supa.table(TABLE).insert(filtered_articles).execute()
 
@@ -62,6 +106,15 @@ class Article:
 
     @classmethod
     def remove_duplicate_articles(cls, articles: list) -> list:
+        """
+        Remove duplicate articles from a list of articles.
+
+            Args:
+                articles (list): A list of article dictionaries.
+
+            Returns:
+                list: A list of unique article dictionaries after removing duplicates.
+        """
         column_articles: list[object] = cls.get_by_column('headline')
         filtered_articles: list[object] = []
         article_headlines = []
@@ -70,7 +123,8 @@ class Article:
             for obj in column_articles:
                 article_headlines.append(obj['headline'])
             if article['headline'] in article_headlines:
-                print(f'Article {article["headline"]} already in database; skipping over this item.')
+                print(
+                    f'Article {article["headline"]} already in database; skipping over this item.')
                 continue
             else:
                 filtered_articles.append(article)
@@ -78,16 +132,15 @@ class Article:
         return filtered_articles
 
     @classmethod
-    def remove_article(cls, article_id: int):
+    def remove(cls, article_id: int):
+        """
+        Remove an article from the database by its ID.
+
+            Args:
+                article_id (int): The ID of the article to be removed.
+
+            Returns:
+                Any: The result of the database operation.
+        """
         data = supa.table(TABLE).delete().eq('id', article_id).execute()
         return data
-
-
-if __name__ == "__main__":
-    test_dict = [{'headline': 'NFL Power Rankings Return 📊',
-                 'link': 'https://bleacherreport.com/articles/10088020-2023-br-nfl-power-rankings-where-does-every-team-stand-entering-week-1',
-                 'image': 'https://media.bleacherreport.com/image/upload/c_crop,h_0.90,w_0.90,x_0.06,y_0.06/v1693952512/hutq42hh56t0pxmhqcnp.jpg',
-                 'summary': 'Where the Bears stand after making some offseason improvements 📲',
-                 'team_name': 'Bears'}]
-
-    print(Article.save(test_dict))
