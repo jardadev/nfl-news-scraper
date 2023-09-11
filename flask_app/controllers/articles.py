327e0e9a9f52f6a472326e4620075a5746dcf09e
AARON JOHNSON <jardaaroncs@gmail.com>
2023-09-11 16:50:52 -0700

    add CRUD api routes

--- flask_app/controllers/articles.py
+++ flask_app/controllers/articles.py
@@ -1,6 +1,7 @@
-from flask import render_template
+from flask import render_template, redirect, jsonify
 from flask_app import app
 from flask_app.models.Article import Article
+from flask_app.helpers.scrape_save import scrape_save
 
 
 @app.route('/')
@@ -16,3 +17,23 @@ def home():
     """
     response = Article.get_all()
     return render_template('index.html', articles=response)
+
+
+@app.route('/api/team/<team_name>')
+def get_team_news(team_name: str):
+    return Article.get_by_team(team_name)
+
+
+@app.route('/api/<int:article_id>')
+def show_article(article_id: int):
+    return Article.get_one(article_id)
+
+
+@app.route('/api/<int:article_id>', methods=['DELETE'])
+def remove_article(article_id: int):
+    return Article.remove(article_id)
+
+
+@app.route('/api/scrape/<team_name>')
+def scrape_articles_by_team(team_name: str):
+    return scrape_save(team=team_name)
