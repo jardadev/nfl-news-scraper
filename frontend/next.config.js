6c3fec2a840f51977d52b87ada62c73923a44fe0
AARON JOHNSON <jardaaroncs@gmail.com>
2023-09-12 13:13:40 -0700

    fix rewrite destination

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
 		return [
 			{
 				source: '/api/:path*',
-				destination: 'http://127.0.0.1:5000/:path*',
+				destination: 'http://127.0.0.1:5000/api/:path*',
 			},
 		];
 	},
