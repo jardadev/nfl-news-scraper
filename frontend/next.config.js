5745df3359a30e583d770c9d131afe996af3bf89
AARON JOHNSON <jardaaroncs@gmail.com>
2023-09-25 23:01:24 -0700

    feat: add BR remote patterns for imgs

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -9,6 +9,22 @@ const nextConfig = {
 			},
 		];
 	},
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'media.bleacherreport.com',
+				port: '',
+				pathname: '/image/**',
+			},
+			{
+				protocol: 'https',
+				hostname: 'i.bleacherreport.net',
+				port: '',
+				pathname: '/images/**',
+			},
+		],
+	},
 };
 
 module.exports = nextConfig;
