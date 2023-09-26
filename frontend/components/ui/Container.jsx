f2f1c03b26934e6bc9ee5713088f3f3dacfbc1c2
AARON JOHNSON <jardaaroncs@gmail.com>
2023-09-25 23:00:28 -0700

    feat: add main container wrapper

--- frontend/components/ui/Container.jsx
+++ frontend/components/ui/Container.jsx
@@ -3,14 +3,14 @@ import { cx } from '@/utils/helpers';
 
 const Container = (props) => {
 	return (
-		<div
+		<main
 			className={cx(
 				'container px-8 mx-auto xl:px-5',
 				props.large ? ' max-w-screen-xl' : ' max-w-screen-lg'
 			)}
 		>
 			{props.children}
-		</div>
+		</main>
 	);
 };
 
