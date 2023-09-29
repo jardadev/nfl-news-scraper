47a57be10dd8547215cda80c90eb788abcc2fa65
AARON JOHNSON <jardaaroncs@gmail.com>
2023-09-28 20:42:38 -0700

    feat: add navbar

---
+++ frontend/components/Navbar.jsx
@@ -0,0 +1,74 @@
+import Link from 'next/link';
+import { useEffect } from 'react';
+import { themeChange } from 'theme-change';
+import { BsFillSunFill } from 'react-icons/bs';
+
+const Navbar = () => {
+	useEffect(() => {
+		themeChange(false);
+	}, []);
+
+	return (
+		<nav className='bg-base-200 backdrop-filter bg-opacity-20 backdrop-blur-lg sticky top-0 z-[20] border-b border-gray-500 py-2 justify-center w-full'>
+			<div className='flex items-center justify-between gap-3 mx-auto max-w-2xl'>
+				<div className='w-fit flex gap-2'>
+					<div className='w-fit'>
+						<input
+							id='my-drawer-3'
+							type='checkbox'
+							className='drawer-toggle'
+						/>
+						{/* Navbar */}
+						<div>
+							<label
+								htmlFor='my-drawer-3'
+								className='btn btn-square btn-ghost'
+							>
+								<svg
+									xmlns='http://www.w3.org/2000/svg'
+									fill='none'
+									viewBox='0 0 24 24'
+									className='inline-block w-6 h-6 stroke-current'
+								>
+									<path
+										strokeLinecap='round'
+										strokeLinejoin='round'
+										strokeWidth='2'
+										d='M4 6h16M4 12h16M4 18h16'
+									></path>
+								</svg>
+							</label>
+						</div>
+						{/* Drawer content */}
+						<div className='drawer-side'>
+							<label
+								htmlFor='my-drawer-3'
+								className='drawer-overlay'
+							></label>
+							<ul className='menu p-4 w-3/4 min-h-full bg-base-200'>
+								{/* Sidebar content here */}
+								{/* TODO */}
+								<li>
+									<a>Sidebar Item 1</a>
+								</li>
+								<li>
+									<a>Sidebar Item 2</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div className='flex items-center'>
+						<Link href={'/'} className='text-2xl'>
+							NFL News 🏈
+						</Link>
+					</div>
+				</div>
+				<div className='btn btn-square btn-ghost'>
+					<BsFillSunFill size={'1rem'} className='text-secondary' />
+				</div>
+			</div>
+		</nav>
+	);
+};
+
+export default Navbar;
