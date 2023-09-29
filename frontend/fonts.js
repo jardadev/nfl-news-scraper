32dfc45231cd5db497efd26d8183d35a29d6072b
AARON JOHNSON <jardaaroncs@gmail.com>
2023-09-28 20:39:16 -0700

    feat: add fonts loader function

---
+++ frontend/fonts.js
@@ -0,0 +1,22 @@
+import { Chewy, Yanone_Kaffeesatz, Khula } from 'next/font/google';
+
+export const chewy = Chewy({
+	weight: '400',
+	subsets: ['latin'],
+	display: 'swap',
+	variable: '--font-chewy',
+});
+
+export const yk = Yanone_Kaffeesatz({
+	weight: '400',
+	subsets: ['latin'],
+	display: 'swap',
+	variable: '--font-yk',
+});
+
+export const khula = Khula({
+	weight: '400',
+	subsets: ['latin'],
+	display: 'swap',
+	variable: '--font-khula',
+});
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,11 @@
-import '@/styles/globals.css'
-
+import '@/styles/globals.css';
+import { chewy, yk, khula } from '@/fonts';
+import Navbar from '@/components/Navbar';
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<main className={`${chewy.variable} ${yk.variable} ${khula.variable} flex flex-col items-center justify-items-center`}>
+			<Navbar />
+			<Component {...pageProps} />
+		</main>
+	);
 }
