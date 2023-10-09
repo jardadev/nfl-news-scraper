3188625bc125df80f95552d2170d86a5087073db
AARON JOHNSON <jardaaroncs@gmail.com>
2023-10-09 16:20:16 -0700

    feat: add Article item component

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,9 +1,5 @@
-import { Inter } from 'next/font/google';
 import Container from '@/components/ui/Container';
-import Navbar from '@/components/Navbar';
 import ArticleItem from '@/components/Articles/ArticleItem';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export default function Home({ articles }) {
 	return (
@@ -11,28 +7,6 @@ export default function Home({ articles }) {
 			<ul className='flex flex-col gap-3 items-center'>
 				{articles &&
 					articles.map(({ headline, image, summary, link }, i) => (
-						// <div
-						// 	className='card lg:card-side bg-base-100 shadow-xl'
-						// 	key={i}
-						// >
-						// 	<figure>
-						// 		<img src={image} alt={headline} />
-						// 	</figure>
-						// 	<div className='card-body'>
-						// 		<h2 className='card-title'>
-						// 			{headline}
-						// 		</h2>
-						// 		{summary && <p>{summary}</p>}
-						// 		<div className='card-actions justify-end'>
-						// 			<a
-						// 				className='btn btn-primary'
-						// 				href={link}
-						// 			>
-						// 				Read
-						// 			</a>
-						// 		</div>
-						// 	</div>
-						// </div>
 						<ArticleItem
 							key={i}
 							headline={headline}
