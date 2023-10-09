2e3cbb54dcb9bb3fa2dcbba4b8bf6a9d1179fbbf
AARON JOHNSON <jardaaroncs@gmail.com>
2023-10-09 16:21:11 -0700

    feat: add trim team name function

--- frontend/utils/helpers.js
+++ frontend/utils/helpers.js
@@ -1,1 +1,12 @@
 export const cx = (...classNames) => classNames.filter(Boolean).join(' ');
+
+export const trimTeamName = (fullName) => {
+	const parts = fullName.split(' ');
+	if (parts.length > 1) {
+		// If there's more than one part, take the last part as the team name
+		return parts.slice(1).join(' ');
+	} else {
+		// If there's only one part, return it as is
+		return fullName;
+	}
+};
