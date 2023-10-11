097be0b438a7cee739de8f19299669f2533c5c96
AARON JOHNSON <jardaaroncs@gmail.com>
2023-10-10 19:50:48 -0700

    style: desktop responsiveness

--- frontend/components/TeamNavList.jsx
+++ frontend/components/TeamNavList.jsx
@@ -5,16 +5,16 @@ import { trimTeamName } from '@/utils/helpers';
 
 const TeamNavList = () => {
 	return (
-		<div className='p-4 w-3/4 min-h-full bg-base-200 text-base-content grid grid-cols-1 gap-2 text-xs'>
+		<div className='p-4 w-3/4 sm:w-1/3 min-h-full bg-base-200 text-base-content grid grid-cols-1 gap-2 text-xs'>
 			{NFL_TEAMS.map((team) => (
 				<div
 					key={team.name}
-					className='flex items-center px-2 hover:bg-gray-200 w-full cursor-pointer'
+					className='flex items-center px-2 hover:bg-gray-200 w-full cursor-pointer sm:justify-between sm:mx-auto'
 				>
-					<div className='self-center'>
+					<div className='xl:pl-12'>
 						<TeamLogo teamName={team.name} />
 					</div>
-					<div className='ml-4'>
+					<div className='ml-4 flex justify-center w-full xl:text-lg'>
 						<Link
 							href={`/news/${trimTeamName(
 								team.name
