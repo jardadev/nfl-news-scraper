30fb7ed1e6fd434c10da835bd0d476d9d52049aa
AARON JOHNSON <jardaaroncs@gmail.com>
2023-10-10 19:51:58 -0700

    style: small refactor and font size

--- frontend/components/ui/Container.jsx
+++ frontend/components/ui/Container.jsx
@@ -1,14 +1,9 @@
 import React from 'react';
-import { cx } from '@/utils/helpers';
+import TeamNavList from '../TeamNavList';
 
 const Container = (props) => {
 	return (
-		<main
-			className={cx(
-				'container py-4 px-8 mx-auto xl:px-5',
-				props.large ? ' max-w-screen-xl' : ' max-w-screen-lg'
-			)}
-		>
+		<main className='container py-4 px-8 mx-auto xl:px-5 lg:text-lg'>
 			{props.children}
 		</main>
 	);
