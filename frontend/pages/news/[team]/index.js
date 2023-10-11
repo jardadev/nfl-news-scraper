45b6a5f3cf1d588416643ddad7522a47fa776d3e
AARON JOHNSON <jardaaroncs@gmail.com>
2023-10-10 21:49:45 -0700

    feat: add team name to top of page

--- frontend/pages/news/[team]/index.js
+++ frontend/pages/news/[team]/index.js
@@ -1,10 +1,14 @@
-
 import Container from '@/components/ui/Container';
 import ArticleItem from '@/components/Articles/ArticleItem';
 
 export default function TeamArticles({ articles }) {
+	const teamName = articles[0].team_name;
+
 	return (
 		<Container>
+			<div className='h-8 pt-4 pb-8 flex items-center justify-center mx-auto'>
+				<h1 className='text-3xl underline'>{teamName}</h1>
+			</div>
 			<ul className='flex flex-col gap-3 items-center'>
 				{articles &&
 					articles.map(({ headline, image, summary, link }, i) => (
@@ -37,5 +41,5 @@ export async function getStaticPaths() {
 export async function getStaticProps({ params }) {
 	const res = await fetch(`http://127.0.0.1:3000/api/team/${params.team}`);
 	const articles = await res.json();
-	return { props: { articles } };
+	return { props: { articles, team: params.team } };
 }
