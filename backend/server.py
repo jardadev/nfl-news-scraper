a32db5772c947a998c4ee65a8ccf1f4538441f96
AARON JOHNSON <jardaaroncs@gmail.com>
2023-10-11 19:19:27 -0700

    refactor: remove flask

--- backend/server.py
+++
@@ -1,7 +0,0 @@
-import os
-from flask_app import app
-from flask_app.controllers import articles
-
-# remove debug argument in production environment
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
