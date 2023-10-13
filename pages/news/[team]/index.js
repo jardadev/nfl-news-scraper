238bb736fb93e67ae0b2536bf7d168cfa062b97a
AARON JOHNSON <jardaaroncs@gmail.com>
2023-10-13 12:29:33 -0700

    fix: remove title casing function

--- pages/news/[team]/index.js
+++ pages/news/[team]/index.js
@@ -41,15 +41,15 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-	function toTitleCase(str) {
-		return str.replace(/\w\S*/g, function (txt) {
-			return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-		});
-	}
+	// function toTitleCase(str) {
+	// 	return str.replace(/\w\S*/g, function (txt) {
+	// 		return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+	// 	});
+	// }
 	const { data: articles, error } = await supabase
 		.from('articles')
 		.select()
-		.eq('team_name', toTitleCase(params.team));
+		.eq('team', params.team);
 
 	if (error) {
 		console.error(error);
