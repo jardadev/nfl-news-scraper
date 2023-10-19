fac810aa735ed3617a5b2f6389eede7dee4640c6
AARON JOHNSON <jardaaroncs@gmail.com>
2023-10-19 14:24:26 -0700

    refactor: move nav components to nav dir

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import '@/styles/globals.css';
 import { chewy, yk, khula } from '@/fonts';
-import Navbar from '@/components/Navbar';
+import Navbar from '@/components/Nav/Navbar';
 export default function App({ Component, pageProps }) {
 	return (
 		<main className={`${chewy.variable} ${yk.variable} ${khula.variable} flex flex-col items-center justify-items-center`}>
